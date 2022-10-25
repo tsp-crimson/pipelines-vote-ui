8dc375e18e425c6dae65eca602c84f8757c8cd3d
Your Name <you@example.com>
2022-10-25 15:10:29 -0400

    new-animals

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 import json
 import requests
 
-# option_a = os.getenv('OPTION_A', u"Cat 🐺")
-# option_b = os.getenv('OPTION_B', u"Dog 🐶")
+option_a = os.getenv('OPTION_A', u"Cat 🐺")
+option_b = os.getenv('OPTION_B', u"Dog 🐶")
 
 # option_a = os.getenv('OPTION_A', u"Penguin 🐧")
 # option_b = os.getenv('OPTION_B', u"Giraffe 🦒")
@@ -16,8 +16,8 @@ import requests
 # option_a = os.getenv('OPTION_A', u"Elephant 🐘")
 # option_b = os.getenv('OPTION_B', u"Frog 🐸")
 
-option_a = os.getenv('OPTION_A', u"Dollar 💵")
-option_b = os.getenv('OPTION_B', u"Euro 💶")
+# option_a = os.getenv('OPTION_A', u"Dollar 💵")
+# option_b = os.getenv('OPTION_B', u"Euro 💶")
 
 hostname = socket.gethostname()
 
