7d1c1fb1db2e4fccb5d9f186205e46a57560647e
Your Name <you@example.com>
2022-10-25 14:47:57 -0400

    new-animals

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Cat 🐺")
-option_b = os.getenv('OPTION_B', u"Dog 🐶")
+# option_a = os.getenv('OPTION_A', u"Cat 🐺")
+# option_b = os.getenv('OPTION_B', u"Dog 🐶")
 
-# option_a = os.getenv('OPTION_A', u"Penguin 🐧")
-# option_b = os.getenv('OPTION_B', u"Giraffe 🦒")
+option_a = os.getenv('OPTION_A', u"Penguin 🐧")
+option_b = os.getenv('OPTION_B', u"Giraffe 🦒")
 
 # option_a = os.getenv('OPTION_A', u"Elephant 🐘")
 # option_b = os.getenv('OPTION_B', u"Frog 🐸")
